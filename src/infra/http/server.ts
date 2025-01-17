08711fc870c171384515bf7916ecde3873f846da
shandryll <shandryll94@gmail.com>
2025-01-17 14:53:44 -0300

    Finish app

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,57 @@
+import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { exportUploadsRoute } from './routes/export-uploads'
+import { getUploadsRoute } from './routes/get-uploads'
+import { uploadImageRoute } from './routes/upload-image'
+import { transformSwaggerSchema } from './transform-swagger-schema'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler) // Validator -> Manipula e valida os dados de entrada da rota
+server.setSerializerCompiler(serializerCompiler) // Serializar -> Manipula e valida os dados de saída da rota
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  console.error(error)
+  return reply.status(500).send({ message: 'Internal server error.' })
+}) // Error Global
+
 server.register(fastifyCors, { origin: '*' })
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload server',
+      version: '1.0.0',
+    },
+  },
+  transform: transformSwaggerSchema,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(uploadImageRoute)
+server.register(getUploadsRoute)
+server.register(exportUploadsRoute)
+
+server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('🚀 HTTP server running!')
 })
